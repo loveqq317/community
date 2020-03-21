d2cfce4a7b3612a59afd25dba915e76f02d78229
loveqq317 <1052318524@qq.com>
2020-03-21 13:11:32 +0800

    init repo

---
+++ src/main/java/life/majiang/community/community/controller/HelloController.java
@@ -0,0 +1,22 @@
+package life.majiang.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @ClassName HelloController
+ * @Description TODO
+ * @Author Q
+ * @Date 2020/3/21 12:44 下午
+ * @Version 1.0
+ **/
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(@RequestParam(name="name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
